12f1519d4a7886c7d8c4d2bfa1232bf1d7783458
JJW-27 <josephjonwilliams@outlook.com>
2023-03-24 15:49:10 +0000

    changes placeholder data to use api call data

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,28 @@ import { DataGrid } from '@mui/x-data-grid';
 
 function App() {
   const [keyword, setKeyword] = useState<string>('');
-  const [reposData, setReposData] = useState<{}[]>([{}]);
+  const [rowsData, setRowsData] = useState<{}[]>([{id:1}]);
   const [numberOfResults, setNumberOfResults] = useState<number>(0);
-
+console.log(rowsData)
   const columns = [
     { field: 'repoName', headerName: 'Repository name', width: 150 },
     { field: 'authorName', headerName: 'Author', width: 150 },
     { field: 'stars', headerName: 'Stars', width: 150 },
   ];
 
-  const rows = [
-    { id: 1, repoName: 'Hello', authorName: 'World', stars: 0 },
-    { id: 2, repoName: 'DataGridPro', authorName: 'is Awesome', stars: 0 },
-    { id: 3, repoName: 'MUI', authorName: 'is Amazing', stars: 0 },
-  ];
-
   const handleSubmit = (e: any) => {
     e.preventDefault();
     return repositorySearch(keyword).then(repos => {
       setNumberOfResults(repos.total_count);
-      setReposData(repos.items);
+      const relevantData = repos.items.map((repo: any) => {
+        return {
+          id: repo.id,
+          repoName: repo.name,
+          authorName: repo.owner.login,
+          stars: repo.stargazers_count,
+        };
+      });
+      setRowsData(relevantData);
     });
   };
 
@@ -42,7 +44,7 @@ function App() {
         <button type="submit">Search</button>
       </form>
       <DataGrid
-        rows={rows}
+        rows={rowsData}
         columns={columns}
         autoHeight={true}
         pageSizeOptions={[25, 50, 75, 100]}
