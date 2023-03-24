323745a7c64ec5f5f9d5588d117f5f9eeee97778
JJW-27 <josephjonwilliams@outlook.com>
2023-03-24 14:14:29 +0000

    adds first basic keyword input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-
+import { useState } from 'react';
 
 function App() {
+  const [keyword, setKeyword] = useState<string>('');
+
   return (
     <div className="App">
-      
+      <form>
+        <label>
+          Keyword:
+          <input
+            value={keyword}
+            onChange={e => {
+              setKeyword(e.target.value);
+            }}
+          ></input>
+        </label>
+      </form>
     </div>
   );
 }
